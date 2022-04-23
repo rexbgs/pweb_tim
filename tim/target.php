10785e62de795bf06e1e8ec2ddf27476ea536fe9
Bagas Dwi K <101783794+rexbgs@users.noreply.github.com>
2022-04-23 23:25:50 +0700

    Add files via upload

---
+++ tim/target.php
@@ -0,0 +1,12 @@
+<?php
+	echo "Username 		: ".$_POST['username']."<br/>";
+	echo "Password 		: ".$_POST['password']."<br/>";
+	echo "Nama 			: ".$_POST['nama']."<br/>";
+	echo "Jenis Kelamin : ".$_POST['kelamin']."<br/>";
+	echo "Hobi 			: ".$_POST['hobi[]']."<br/>";
+	echo "Provinsi 		: ".$_POST['provinsi']."<br/>";
+	echo "TGL Lahir 	: ".$_POST['lahir']."<br/>";
+	echo "usia	 		: ".$_POST['usia']."<br/>";
+	echo "alamat		: ".$_POST['alamat']."<br/>";
+	echo "Foto			: ".$_POST['foto']."<br/>";
+?>
